58f157c1cdcd2aa6a50ca652e62443d60b203dd6
pythagoras58 <solomondanso58@gmail.com>
2022-09-26 21:31:17 +0000

    control statements

---
+++ src/control_logic/If_Statements.java
@@ -0,0 +1,14 @@
+package control_logic;
+
+public class If_Statements {
+    public static void main(String[] args) {
+
+        boolean isOn = false;
+
+        if(isOn)
+            System.out.println("[isOn]");
+
+        else
+            System.out.println("[NOT isOn]");
+    }
+}
