cccb9f0af0b2dbd8b12d6ad71fe2cef9c8d5c65d
pythagoras58 <solomondanso58@gmail.com>
2022-09-29 21:14:56 +0000

    while loops

---
+++ src/loops/whileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i =0;
+
+        while (i<5){
+            System.out.println("Hi -> " + i);
+            i++;
+        }
+
+    }
+}
