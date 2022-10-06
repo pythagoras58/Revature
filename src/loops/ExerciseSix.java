00b265d1664186052cd6c042ddbd316d6fd213c5
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:25:36 +0000

    Exercise Six

---
+++ src/loops/ExerciseSix.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class ExerciseSix {
+    public static void main(String[] args) {
+        String[] cities = {"Atlanta", "Charlotte", "Dallas", "Los Angeles", "New York", "Orlando", "Philadelphia", "Seattle"};
+
+        for(int i=0; i<cities.length; i++){
+            System.out.println("[CITY]-->" + cities[i]);
+        }
+    }
+}
