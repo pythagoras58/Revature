1ac806802b31328eddaa5c0510f40d1e18e9b1e6
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 21:57:40 +0000

    DO WHILE

---
+++ src/loops/doWhile.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class doWhile {
+    public static void main(String[] args) {
+        boolean on = false;
+
+        do{
+            System.out.println("[ON is ]-->" + on);
+        }while (on);
+    }
+}
