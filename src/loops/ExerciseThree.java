d29b72389e3ff9e26d93c10f6f570986a0869325
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:01:01 +0000

    Exercie 3

---
+++ src/loops/ExerciseThree.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        int number = 1;
+
+        do {
+            System.out.println(number);
+            number ++;
+        }while (number <=49);
+    }
+}
