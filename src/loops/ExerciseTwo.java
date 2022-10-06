e9c1d8d13e30b2cffb8684df03ffc1bf84d6c02c
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 21:49:15 +0000

    LOOP2 RENEW

--- src/loops/ExerciseTwo.java
+++ src/loops/ExerciseTwo.java
@@ -1,8 +1,10 @@
 package loops;
 
 /*
-
-* **/
+* For this exercise, you must write a while loop
+* that prints all of the even numbers 100 to -100, i
+* nclusively, each on a separate line
+* */
 
 public class ExerciseTwo {
     public static void main(String[] args) {
