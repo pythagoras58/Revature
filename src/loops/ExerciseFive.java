3ae155074e289bce6038f827f5bb3d7a0fd9fee8
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:20:43 +0000

    Exercise Five

---
+++ src/loops/ExerciseFive.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class ExerciseFive {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,5,6,7,8,9,10,11};
+
+        for (int i=0; i < arr.length; i++){
+            System.out.println("[ARRAY ELEMENT]-->" + arr[i]);
+        }
+    }
+}
