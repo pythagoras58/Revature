c6758e3ab7ad6b61654270a5b18fd6ecd0e25e09
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:07:51 +0000

    Exercie 4 Review

--- src/loops/ExerciseFour.java
+++ src/loops/ExerciseFour.java
@@ -11,3 +11,9 @@ public class ExerciseFour {
         }while (alphabet <= lastAlphabet);
     }
 }
+
+/*
+* For this exercise, you must write a do-while loop that prints the letters of the alphabet.
+* You must use a single char variable that you manipulate to print to the console.
+* (You shouldn’t have 26 lines of code that each print a letter...)
+* */
