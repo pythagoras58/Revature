2c902311b794261f9e44947971c7446de0c61a9f
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:12:10 +0000

    FOR LOOP

---
+++ src/loops/forLoop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class forLoop {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,2,1};
+
+        for (int i=0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
