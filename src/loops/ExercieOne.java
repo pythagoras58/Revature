0ea3879ec8b9db4f80783a23ef43e4029f51a3a1
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 21:46:32 +0000

    LOOP 2

--- src/loops/ExercieOne.java
+++ src/loops/ExercieOne.java
@@ -1,2 +1,12 @@
-package loops;public class ExercieOne {
+package loops;
+
+public class ExercieOne {
+    public static void main(String[] args) {
+        int counter = 1;
+
+        while(counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
 }
---
+++ src/loops/ExerciseTwo.java
@@ -0,0 +1,18 @@
+package loops;
+
+/*
+
+* **/
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        int number = -100;
+
+        while (number <=100){
+            if(number % 2 == 0){
+                System.out.println(number);
+            }
+            number++;
+        }
+    }
+}
