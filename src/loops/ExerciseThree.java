08fe0696d1da9a9ea097b4f9020e99555c6a264b
pythagoras58 <solomondanso58@gmail.com>
2022-10-06 22:02:34 +0000

    Exercie 3 Review

--- src/loops/ExerciseThree.java
+++ src/loops/ExerciseThree.java
@@ -10,3 +10,9 @@ public class ExerciseThree {
         }while (number <=49);
     }
 }
+
+/*
+* For this exercise, you need to write a do-while loop
+* that prints the odd numbers 1 through 49,
+* inclusively, each on a separate line.
+* */
