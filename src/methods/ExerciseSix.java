2a2b20bec7e446744f813142ce42dd7be40d7043
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 14:09:57 +0000

    method exercise 6.1

--- src/methods/ExerciseSix.java
+++ src/methods/ExerciseSix.java
@@ -1,4 +1,17 @@
 package methods;
 
 public class ExerciseSix {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int c = 3;
+        int d = 1;
+        var display = new ExerciseSix().squred(a,b,c,d);
+        System.out.println(display);
+    }
+
+    public double[] squred(int _one, int _two, int _three, int _four){
+        double[] res = {_one*_one, _two*_two, _three*_three, _four*_four};
+        return res;
+    }
 }
