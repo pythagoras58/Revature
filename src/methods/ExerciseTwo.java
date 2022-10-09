45ebc2953032f0ad03528a82596430a69687276b
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 13:26:30 +0000

    Methods Exercise Two 2

--- src/methods/ExerciseTwo.java
+++ src/methods/ExerciseTwo.java
@@ -1,4 +1,12 @@
 package methods;
 
 public class ExerciseTwo {
+    public static void main(String[] args) {
+        var sayIt = sayHello();
+        System.out.println(sayIt);
+    }
+
+    public static String sayHello(){
+        return "Hello";
+    }
 }
