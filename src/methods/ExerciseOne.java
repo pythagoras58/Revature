15cfe96db4118945bcdc371e684b28dfefa5708c
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 13:10:39 +0000

    Methods Exercise One

--- src/methods/ExerciseOne.java
+++ src/methods/ExerciseOne.java
@@ -1,7 +1,20 @@
 package methods;
 
-public class ExerciseOne{
-        public static void main(String[] args) {
+import java.util.Scanner;
 
-        }
+public class ExerciseOne {
+    public static void main(String[] args) {
+        System.out.println("[ENTER A WORD]");
+        Scanner scanner = new Scanner(System.in);
+        String textWord = scanner.next();
+
+        var myMethod = new ExerciseOne().sayTextWord(textWord);
+
+        System.out.println("[WORD WAS]->" + myMethod);
+
+    }
+
+    public String sayTextWord(String _textWord){
+        return _textWord;
+    }
 }
