de9bb45700434f487aa61ee5295311cc41b84180
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 14:04:39 +0000

    method exercise 6

--- src/methods/ExerciseFive.java
+++ src/methods/ExerciseFive.java
@@ -1,4 +1,17 @@
 package methods;
 
 public class ExerciseFive {
+    public static void main(String[] args) {
+        getAlphabetArray();
+    }
+
+    public static void getAlphabetArray(){
+        char first = 'a';
+        char last = 'z';
+
+        do{
+            System.out.println(first);
+            first++;
+        }while(first <= last);
+    }
 }
