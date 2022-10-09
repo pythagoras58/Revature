020cef84409c4dbe7c3ae7ded4cd2c6f20fe0024
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 13:37:03 +0000

    Methods Exercise Three 2

--- src/methods/ExerciseThree.java
+++ src/methods/ExerciseThree.java
@@ -1,4 +1,11 @@
 package methods;
 
 public class ExerciseThree {
+    public static void main(String[] args) {
+        myText();
+    }
+
+    public static void myText(){
+        System.out.println("I LOVE ANNE");
+    }
 }
