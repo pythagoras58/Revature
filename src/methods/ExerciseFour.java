e048399e3def0bd3746ccb09c4a673c998f00225
pythagoras58 <solomondanso58@gmail.com>
2022-10-09 13:42:03 +0000

    Methods Exercise Four 2

--- src/methods/ExerciseFour.java
+++ src/methods/ExerciseFour.java
@@ -1,4 +1,14 @@
 package methods;
 
+import java.util.Scanner;
+
 public class ExerciseFour {
+    public static void main(String[] args) {
+       getPi();
+    }
+
+    public static void getPi(){
+        var calc = Math.PI;
+        System.out.println(calc);
+    }
 }
