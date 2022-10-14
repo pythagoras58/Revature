f448330b26ca7d61c7238fbc045f60f6d39b61a6
pythagoras58 <solomondanso58@gmail.com>
2022-10-14 20:34:44 +0000

    algorithm - Merge 5

--- src/algorithm/MergeSort.java
+++ src/algorithm/MergeSort.java
@@ -7,6 +7,7 @@ public class MergeSort {
         char[] letters = {'a','b','c','d','e','f', 'g', 'h', 'i', 'j', 'k',
                 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+        System.out.println(findLetterFromMiddle(letter,letters));
     }
 
     public static int findIndx(char target, char[] data){
