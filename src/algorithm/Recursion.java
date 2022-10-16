890985ab06079aef0ad1a8945ce47a300506be11
pythagoras58 <solomondanso58@gmail.com>
2022-10-16 21:26:36 +0000

    Recursion 2

--- src/algorithm/Recursion.java
+++ src/algorithm/Recursion.java
@@ -2,6 +2,18 @@ package algorithm;
 
 public class Recursion {
     public static void main(String[] args) {
+        int input = 5;
+
+        System.out.println(factorial(input));
+
+    }
+
+    public static int factorial(int num){
+        if(num <= 0)
+            return 0;
+        if(num == 1)
+            return 1;
+        return num * factorial(num);
 
     }
 }
