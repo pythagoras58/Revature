4f2befdfff24b5ad3d6fcaccd23f99c99f117525
pythagoras58 <solomondanso58@gmail.com>
2022-10-29 16:45:27 +0000

    Scanner Exercise 2 2

--- src/userInput/ExerciseTwo.java
+++ src/userInput/ExerciseTwo.java
@@ -1,4 +1,12 @@
 package userInput;
 
+import java.util.Scanner;
+
 public class ExerciseTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER SOMETHING");
+        String line = scanner.nextLine();
+        System.out.println("YOU ENTERED : " + line);
+    }
 }
