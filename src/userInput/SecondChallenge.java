b68ba0cbaa9b70875e8afb096dbf039f96002af2
pythagoras58 <solomondanso58@gmail.com>
2022-10-29 18:07:20 +0000

    Scanner Challenge 6

--- src/userInput/SecondChallenge.java
+++ src/userInput/SecondChallenge.java
@@ -1,15 +1,36 @@
 package userInput;
 
+import java.util.Scanner;
+
 public class SecondChallenge {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER A CHARACTER");
+        String line = scanner.nextLine();
+        moverFunc(line);
     }
 
-    public static boolean isVowel(char letter){
-        if(letter == 'a'|| letter =='e'||  letter =='i' || letter =='o' || letter =='u'){
-            return true;
-        }else{
-            return false;
+    public static String moverFunc(String myInput){
+        switch (myInput){
+            case "1":
+                System.out.println("Moving Right");
+                break;
+            case "2":
+                System.out.println("Moving Left");
+                break;
+            case "3":
+                System.out.println("Moving Up");
+                break;
+            case "4":
+                System.out.println("Moving Down");
+                break;
+            case "q":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Try Again, Unacceptable Input");
+                break;
         }
+        return myInput;
     }
 }
