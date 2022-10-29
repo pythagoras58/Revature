67993162c9dba1ecd6d0abbb62ec753eaefb42aa
pythagoras58 <solomondanso58@gmail.com>
2022-10-29 16:31:30 +0000

    Scanner Complete

--- src/userInput/ExampleOne.java
+++ src/userInput/ExampleOne.java
@@ -1,4 +1,14 @@
 package userInput;
 
+import java.util.Scanner;
+
 public class ExampleOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String results = "";
+        System.out.println("ENTER A STRING");
+        results = scanner.nextLine();
+        scanner.close();
+        System.out.println("RESULT: " + results);
+    }
 }
