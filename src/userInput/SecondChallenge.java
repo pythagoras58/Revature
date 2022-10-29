40b5214b7e685391937427a68d549922066474d1
pythagoras58 <solomondanso58@gmail.com>
2022-10-29 17:54:52 +0000

    Scanner Challenge 5

--- src/userInput/SecondChallenge.java
+++ src/userInput/SecondChallenge.java
@@ -4,4 +4,12 @@ public class SecondChallenge {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isVowel(char letter){
+        if(letter == 'a'|| letter =='e'||  letter =='i' || letter =='o' || letter =='u'){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
