33e27bd432b9d3b78cc08b734e4c5db11987d01d
pythagoras58 <solomondanso58@gmail.com>
2022-10-30 21:54:11 +0000

    classes 3

--- src/userClasses/ClassOneTest.java
+++ src/userClasses/ClassOneTest.java
@@ -1,4 +1,9 @@
 package userClasses;
 
 public class ClassOneTest {
+    public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+        classOne.setObjectId(23);
+        System.out.println(classOne.getObjectId());
+    }
 }
