94dbc26dd3c9baed94c6f7950afba3304ff19691
pythagoras58 <solomondanso58@gmail.com>
2022-10-30 21:51:18 +0000

    classes 1

--- src/userClasses/ClassOne.java
+++ src/userClasses/ClassOne.java
@@ -1,11 +1,18 @@
 package userClasses;
 
 public class ClassOne {
-    public static void main(String[] args) {
-
-    }
 
     private int objectId;
 
+    public int getObjectId() {
+        return objectId;
+    }
 
+    public void setObjectId(int objectId) {
+        this.objectId = objectId;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
