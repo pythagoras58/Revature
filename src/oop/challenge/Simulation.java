344e99293ce2fb7e81c74506c89745acfb8a6a2e
pythagoras58 <solomondanso58@gmail.com>
2023-01-01 22:53:17 +0000

    Composite Challenge - Complete

--- src/oop/challenge/Simulation.java
+++ src/oop/challenge/Simulation.java
@@ -1,4 +1,8 @@
 package oop.challenge;
 
 public class Simulation {
+    public static void main(String[] args) {
+        WeatherMachine weatherMachine = new WeatherMachine();
+        weatherMachine.getWeatherReport();
+    }
 }
