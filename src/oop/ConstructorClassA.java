9f08cdcd25a86f86d42ab235ac3030600139a273
pythagoras58 <solomondanso58@gmail.com>
2023-01-01 21:46:45 +0000

    Contructors 1

--- src/oop/ConstructorClassA.java
+++ src/oop/ConstructorClassA.java
@@ -1,4 +1,12 @@
 package oop;
 
 public class ConstructorClassA {
+
+    public ConstructorClassA() {
+        System.out.println("HI");
+    }
+
+    public static void main(String[] args) {
+        ConstructorClassA A = new ConstructorClassA();
+    }
 }
