4a8dd81026275f435b6cd8b19ca4f348ba9d0be9
pythagoras58 <solomondanso58@gmail.com>
2023-01-01 22:51:29 +0000

    Composite Challenge - Begin Weather class 2

--- src/oop/challenge/WeatherMachine.java
+++ src/oop/challenge/WeatherMachine.java
@@ -15,5 +15,23 @@ public class WeatherMachine {
         thermometer = new Thermometer();
     }
 
+    private double[] getPastTemperatures(){
+        double[] result = new double[24];
+
+        for(int i = 0; i < result.length; i++) {
+            result[i] = thermometer.getTemperature();
+        }
+
+        return result;
+    }
+
+    public void getWeatherReport() {
+        System.out.println("Your last 24 hours:");
+        double[] temps = getPastTemperatures();
+
+        for(int i = 0; i < temps.length; i++) {
+            System.out.println(i + ":00 " + temps[i] + " degrees");
+        }
+    }
 
 }
