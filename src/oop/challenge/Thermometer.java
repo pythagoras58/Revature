dccbf1574cfe3541e37a5b8f802fcb58cc11c77d
pythagoras58 <solomondanso58@gmail.com>
2023-01-01 22:40:52 +0000

    Composite Challenge 1

--- src/oop/challenge/Thermometer.java
+++ src/oop/challenge/Thermometer.java
@@ -1,4 +1,9 @@
 package oop.challenge;
 
 public class Thermometer {
+    /*
+    * Thermometer should have one public method getTemperature()
+    * that returns a random value between 0 (degrees Fahrenheit)
+    * and 108 (degrees Fahrenheit).
+     */
 }
