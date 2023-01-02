216e0ef92b8e58cb43eeb40ea4daf17b85d3b039
pythagoras58 <solomondanso58@gmail.com>
2023-01-02 07:42:58 +0000

    Composite Challenge - Completion

--- src/oop/challenge/Simulation.java
+++ src/oop/challenge/Simulation.java
@@ -1,6 +1,11 @@
 package oop.challenge;
 
 public class Simulation {
+    /*
+    The Simulation class should only declare a main method.
+    Inside of main you should create an instance of
+    WeatherMachine and call the getWeatherReport() method from it
+     */
     public static void main(String[] args) {
         WeatherMachine weatherMachine = new WeatherMachine();
         weatherMachine.getWeatherReport();
