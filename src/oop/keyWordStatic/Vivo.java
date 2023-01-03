06f994213fc7102387250b32a966742d26aacf57
pythagoras58 <solomondanso58@gmail.com>
2023-01-03 23:24:57 +0000

    static Keyword 2

--- src/oop/keyWordStatic/Vivo.java
+++ src/oop/keyWordStatic/Vivo.java
@@ -1,4 +1,11 @@
 package oop.keyWordStatic;
 
 public class Vivo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void getVal(){
+        System.out.println("Value Activated");
+    }
 }
