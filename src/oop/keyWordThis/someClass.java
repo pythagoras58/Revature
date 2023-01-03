82757686c0947a19400e5828295bdedea93b110a
pythagoras58 <solomondanso58@gmail.com>
2023-01-03 23:06:27 +0000

    This keyword

---
+++ src/oop/keyWordThis/someClass.java
@@ -0,0 +1,4 @@
+package oop.keyWordThis;
+
+public class someClass {
+}
--- src/oop/objectAndClasses/Simulator.java
+++ src/oop/objectAndClasses/Simulator.java
@@ -4,5 +4,11 @@ public class Simulator {
 
     public static void main(String[] args) {
 
+        // instantiate the Gadget class
+        Gadget gadget = new Gadget();
+        Gadget gadget1 = new Gadget();  
+
+        System.out.println("Serial Number of Gadget 1 -->" + gadget.serialNumber);
+        System.out.println("Serial Number of Gadget 2 -->" + gadget1.serialNumber);
     }
 }
