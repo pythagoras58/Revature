923597ec037e1c17f67177fb129668bc8c9dbd23
pythagoras58 <solomondanso58@gmail.com>
2023-01-03 23:09:11 +0000

    This keyword 2

--- src/oop/keyWordThis/someClass.java
+++ src/oop/keyWordThis/someClass.java
@@ -4,9 +4,11 @@ public class someClass {
     long id;
 
     public someClass(){
+        //this acting as a method
     }
 
     public someClass(long id){
+        // this acting as the current object finder
         this.id = id;
     }
 }
