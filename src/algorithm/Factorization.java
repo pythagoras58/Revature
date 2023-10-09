1a82cd7b9274359af29cb1035815546f5950e7cc
pythagoras58 <solomondanso58@gmail.com>
2023-10-09 12:56:52 +0800

    Factorization chalenge - 1

--- src/algorithm/Factorization.java
+++ src/algorithm/Factorization.java
@@ -1,7 +1,25 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Factorization {
     public static void main(String[] args) {
+        int number;
+        int[] factors = {};
+        // accept the number from the user
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("====KIndly Input A Number ====");
+        number = scan.nextInt();
+
+        for(int i=1; i<= number; i++){
+            // If number is divided by i :: i becomes the factor
+
+            if(number % i == 0){
+
+                System.out.println("The factors of :" + number + " is: " + i);
+            }
+        }
     }
 }
